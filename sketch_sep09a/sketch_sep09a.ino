9c9cf80bbe39564ba14148b2c726062b511091ad
IMakhonin24ru <imakhonin24ru@gmail.com>
2021-09-12 14:56:15 +0700

    Add led

--- sketch_sep09a/sketch_sep09a.ino
+++ sketch_sep09a/sketch_sep09a.ino
@@ -1,15 +1,24 @@
+boolean buttonFlag = 0;
+boolean button;
+boolean ledFlag = 0;
+unsigned long lastPress;
+
 void setup() {
-  pinMode(5, INPUT_PULLUP);
-  pinMode(13, OUTPUT);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(4, OUTPUT);
 }
 
 void loop() {
-  boolean button = !digitalRead(5);
-  
-  if(!!button){
-      digitalWrite(13, HIGH);
-  }else{
-      digitalWrite(13, LOW);
+  boolean button = !digitalRead(3);
+
+  if(button == 1 && buttonFlag == 0 && millis()-lastPress > 100){
+      buttonFlag = 1;
+      ledFlag = !ledFlag;
+      digitalWrite(4, ledFlag);
+      lastPress = millis();
   }
-  
+
+  if(button == 0 && buttonFlag == 1){
+      buttonFlag = 0;
+  }
 }
