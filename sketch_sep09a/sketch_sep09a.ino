6a6a38046eb89d3e91ad793a736a82a63c7488f3
IMakhonin24ru <imakhonin24ru@gmail.com>
2021-09-12 15:33:23 +0700

    Third version

--- sketch_sep09a/sketch_sep09a.ino
+++ sketch_sep09a/sketch_sep09a.ino
@@ -1,24 +1,17 @@
-boolean buttonFlag = 0;
-boolean button;
-boolean ledFlag = 0;
-unsigned long lastPress;
+unsigned int lightSensor;
+int criticalLightVal = 720;
 
 void setup() {
-  pinMode(3, INPUT_PULLUP);
+  Serial.begin(9600);
   pinMode(4, OUTPUT);
 }
 
 void loop() {
-  boolean button = !digitalRead(3);
+  lightSensor = analogRead(7);
 
-  if(button == 1 && buttonFlag == 0 && millis()-lastPress > 100){
-      buttonFlag = 1;
-      ledFlag = !ledFlag;
-      digitalWrite(4, ledFlag);
-      lastPress = millis();
+  if (lightSensor < criticalLightVal){
+    digitalWrite(4, HIGH);
+  }else{
+    digitalWrite(4, LOW);
   }
-
-  if(button == 0 && buttonFlag == 1){
-      buttonFlag = 0;
-  }
-}
+} 
